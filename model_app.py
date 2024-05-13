180c589a0352b522ce01867d4f964b53c498c95f
harrrang <berix0218@naver.com>
2024-05-13 22:05:53 +0900

    refactor: model_app.py 수정

--- model_app.py
+++ model_app.py
@@ -3,15 +3,22 @@ from joblib import load
 
 app = Flask(__name__)
 
+# joblib 파일로부터 모델 불러오기
+model = load('tam.joblib')
+
+#클라이언트에게 받은 변수 모델에 맞는 형식으로 바꾸는 함수
+def model_parameter(input):
+    #TODO:로직구현해야함
+    return 0
+
 # 예측 모델 함수
-def predict_model(something):
-    # 여기 모델 구현 or 연결
-    result = something
+def predict_model(data1, data2):
+    result = model.predict([[data1, data2]])
     return result
 
 @app.route('/')
 def hello_world():
-  return 'Hello,  My name is UMP. I have BIG muscle. I am strong man HA!HA!HA!'
+  return 'Hello,  My name is EOM. I have BIG muscle. I am strong man HA!HA!HA!'
 
 @app.route('/predict', methods=['POST'])
 def predict():
